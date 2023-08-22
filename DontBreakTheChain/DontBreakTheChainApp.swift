9e7a727a88e4c086160f41e6363634072446fc4d
Richy <garrincha33@users.noreply.github.com>
2023-08-22 08:26:46 +0100

    Initial Commit

---
+++ DontBreakTheChain/DontBreakTheChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontBreakTheChainApp.swift
+//  DontBreakTheChain
+//
+//  Created by Richard Price on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DontBreakTheChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
