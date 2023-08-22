8f3283429f95045c6effa38d4b74140daf64864f
Richy <garrincha33@users.noreply.github.com>
2023-08-22 08:28:45 +0100

    inital commit

--- DontBreakTheChain/ContentView.swift
+++ DontBreakTheChain/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColßor(.accentColor)
+            Text("Hello, Test!")
         }
         .padding()
     }
