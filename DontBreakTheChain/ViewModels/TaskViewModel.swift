9b22055ee5358069ce3ae426aeb3ff91e0540549
Richy <garrincha33@users.noreply.github.com>
2023-08-22 11:05:22 +0100

    added storage and updated some logic and views for better presentation

--- DontBreakTheChain/ViewModels/TaskViewModel.swift
+++ DontBreakTheChain/ViewModels/TaskViewModel.swift
@@ -7,22 +7,57 @@
 
 import Foundation
 
-final class TaskViewModel: ObservableObject, TaskManaglable {
+final class TaskViewModel: ObservableObject {
+    @Published var tasks: [Task] = []
     
-    @Published private var tasks: [Task] = []
+    private let storage: Storageable
+    private let tasksKey = "tasksKey"
+    
+    init(storage: Storageable = UserDefaultsStorage()) {
+        self.storage = storage
+        loadTasks()
+    }
     
     func addTask(_ task: Task) {
         tasks.append(task)
+        saveTasks()
     }
     
     func markTaskComplete(at index: Int) {
-        tasks[index].isCompleted = true
-        tasks[index].completionCount += 1
+        tasks[index].isCompleted.toggle()
+        if tasks[index].isCompleted {
+            tasks[index].completionCount += 1
+        } else {
+            tasks[index].completionCount -= 1
+        }
+        saveTasks()
     }
 
+    private func saveTasks() {
+        storage.save(tasks, for: tasksKey)
+    }
+    
+    private func loadTasks() {
+        if let loadedTasks: [Task] = storage.load([Task].self, with: tasksKey) {
+            self.tasks = loadedTasks
+        }
+    }
     
     func getAllTasks() -> [Task] {
         return tasks
     }
+    
+    func increaseCompletionCount(at index: Int) {
+            tasks[index].completionCount += 1
+            tasks[index].isCompleted = tasks[index].completionCount == 7
+            saveTasks()
+        }
+    
+    func decreaseCompletionCount(to value: Int, at index: Int) {
+        tasks[index].completionCount = value
+        tasks[index].isCompleted = tasks[index].completionCount == 7
+        saveTasks()
+    }
+
 
 }
